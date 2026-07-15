7602ceac4cc341aa315acba468935cf058f4b52d
einliterflasche <einliterflasche@pm.me>
2026-07-15 19:36:07 +0200

    FORK-PATCH: refresh Nix development environment

--- shell.nix
+++ shell.nix
@@ -9,4 +9,12 @@ pkgs.mkShell {
     pkg-config
     gnumake
   ];
+
+  shellHook = ''
+    stamp=node_modules/.package-lock.stamp
+    if [ ! -d node_modules ] || ! cmp -s package-lock.json "$stamp"; then
+      echo "shell.nix: package-lock.json changed, running npm ci..."
+      npm ci && cp package-lock.json "$stamp"
+    fi
+  '';
 }
